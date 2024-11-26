81dfe300004254fb96454152e9bfd3f1da406ed6
iamcelestino <trcelestino488@gmail.com>
2024-11-26 02:13:49 -0800

    feat: show message in home page

---
+++ App/core/config.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Core;
+
+define('BASE_URL', 'http://localhost/VAGA/');
+define('HOST', 'localhost');
+define('DB_NAME', 'app_vaga');
+define('USER', 'root');
+define('DB_DRIVER', 'mysql');
+define('PASSWORD', '');
